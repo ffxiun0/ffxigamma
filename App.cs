1623b328d3b6bb4e073a7e3ca5b4fbd72c32851b
ffxiun0 <ffxiun0@gmail.com>
2022-02-13 19:10:23 +0900

    v2.3

--- App.cs
+++ App.cs
@@ -17,7 +17,7 @@ using System.Windows.Forms;
 
 namespace ffxigamma {
     public partial class App : Form {
-        public const string Version = "2.2.1";
+        public const string Version = "2.3";
         public const string AppName = "FFXI Gamma";
         private const string AppFolderName = "ffxigamma";
         private const string ConfigFileName = "config.xml";
